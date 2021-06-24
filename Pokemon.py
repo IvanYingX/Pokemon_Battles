7f42cb342f3c9d1a961fb203f88c678a047dc3fc
Ivan Ying Xuan <ying.yingxuan@hotmail.com>
2021-06-24 17:56:08 +0200

    Clearer interface

--- Pokemon.py
+++ Pokemon.py
@@ -1,30 +1,41 @@
 class Pokemon:
-    def __init__(self, hp, name, attack, defense, moves=None):
+    def __init__(self, hp, name, attack, defense, elemental_type, moves=None):
         self.hp = hp
         self.name = name
         self.attack = attack
         self.defense = defense
+        self.elemental_type = elemental_type
         if moves is None:
             self.moves = []
             self.moves.append(Attack('Pound', 40, 30, 30, 'Normal'))
         else:
             self.moves = moves
         self.move_names = [x.name for x in self.moves]
+
     def fight(self, other):
-        
         while True:
             print(f'{self.name} has the following moves')
-            for i in self.move_names:
-                print(i)
-            move = input(f"What should {self.name} use? \n")
+            print('\t'.join(self.move_names))
+            move = input(f"\nWhat should {self.name} use?\t")
             if move not in self.move_names:
                 print(f'{self.name} doesn\'t know that move')
             else:
                 break
         idx = self.move_names.index(move)
         move = self.moves[idx]
-        other.hp -= self.attack * move.power
+        if move.elemental_type == 'Fire' and other.elemental_type == 'Grass':
+            other.hp -= 2 * (self.attack * move.power) / other.defense
+            print("It's super effective!")
+        else:
+            other.hp -= (self.attack * move.power) / other.defense
+
         move.pp -= 1
+        if other.hp <= 0:
+            other.hp = 0
+            print(f'{other.name} has fainted')
+            return False
+
+        return True
 
 class Attack:
     def __init__(self, name, power, pp, max_pp, elemental_type):
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from Pokemon import Pokemon
 from Pokemon import Attack
+import time
 
-pikachu = Pokemon(200, 'Pikachu', 30, 20)
-bulbasaur = Pokemon(200, 'Bulbasaur', 30, 20)
-pikachu.fight(bulbasaur)
+thunder = Attack('Thunder', 150, 5, 5, 'Electric')
+pound = Attack('Pound', 40, 30, 30, 'Normal')
+ember = Attack('Ember', 40, 20, 20, 'Fire')
+vine_whip = Attack('Vine Whip', 45, 25, 25, 'Grass')
+pikachu_moveset = [pound, thunder, ember]
+bulbasaur_moveset = [pound, vine_whip]
+
+pikachu = Pokemon(200, 'Pikachu', 30, 20, 'Electric', moves=pikachu_moveset)
+bulbasaur = Pokemon(200, 'Bulbasaur', 30, 20, 'Grass', moves=bulbasaur_moveset)
+
+while True:
+    flag = pikachu.fight(bulbasaur)
+    if flag == False:
+        break
+    print(f'{bulbasaur.name} has {bulbasaur.hp} points left\n')
+    time.sleep(2)
+    flag = bulbasaur.fight(pikachu)
+    if flag == False:
+        break
+    print(f'{pikachu.name} has {pikachu.hp} points left\n')
+    
