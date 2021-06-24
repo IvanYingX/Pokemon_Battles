79fb7b1d1e053e9d3f439c8bcbc182b0e9ca2174
yuyacademia@gmail.com <yuyacademia@gmail.com>
2021-06-25 01:00:21 +0900

    yuya's pokemon code

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from Pokemon import Pokemon
 from Pokemon import Attack
 
-pikachu = Pokemon(200, 'Pikachu', 30, 20)
-bulbasaur = Pokemon(200, 'Bulbasaur', 30, 20)
-pikachu.fight(bulbasaur)
+Pound = Attack('Pound', 40, 30, 30, 'Normal')
+FireBlast = Attack('Fire Blast', 100, 10, 10, 'Fire')
+Thunder = Attack('Thunder', 120, 5, 5, 'Electric')
+pikachu_moves=[Pound, FireBlast, Thunder]
+
+pikachu = Pokemon(200, 'Pikachu', 30, 20, 'electric', pikachu_moves)
+bulbasaur = Pokemon(200, 'Bulbasaur', 30, 20, 'grass')
+
+print(pikachu.__dir__())
+while True:
+    if pikachu.hp>0:
+        pikachu.fight(bulbasaur)
+    else:
+        print("Pikachu is incapacitated!")
+    if bulbasaur.hp>0:
+        bulbasaur.fight(pikachu)
+    else:
+        print("Bulbasaur is incapacitated!")
+        break
