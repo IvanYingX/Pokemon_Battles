c1f7c50d6ce9c5a285b7a32bdf613c051282d886
Ivan Ying Xuan <ying.yingxuan@hotmail.com>
2021-07-05 16:55:57 +0100

    Clean Project

---
+++ Game/Pokemon.py
@@ -0,0 +1,95 @@
+from gui import attack_gui
+import time
+import random
+import sys
+
+def slow_print(message: str, typing_speed=200):
+    for char in message:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(random.random() * 10.0/typing_speed)
+    print('')
+
+class Pokemon:
+    def __init__(self, hp: int, name: str,
+                 attack: int, defense: int,
+                 elemental_type: str, moves: list = None):
+        self.hp = hp
+        self.name = name
+        self.attack = attack
+        self.defense = defense
+        self.elemental_type = ElementalType(elemental_type)
+        if moves is None:
+            self.moves = []
+            self.moves.append(Attack('Pound', 40, 30, 'Normal'))
+        else:
+            if len(moves) > 4:
+                raise ValueError('Pokemon can\'t learn more than four moves')
+            else:
+                self.moves = moves
+        self.move_names = [x.name for x in self.moves]
+
+    def fight(self, other):
+        idx = attack_gui(self.move_names)
+        move = self.moves[idx]
+        slow_print(f'{self.name} used {move}')
+        if move.elemental_type.element == 'Normal' or other.elemental_type.element == 'Normal':
+            other.hp -= (self.attack * move.power) / other.defense
+        elif move.elemental_type > other.elemental_type:
+            other.hp -= 2 * (self.attack * move.power) / other.defense
+            slow_print("It's super effective!")
+        elif move.elemental_type < other.elemental_type or move.elemental_type == other.elemental_type:
+            other.hp -= 0.5 * (self.attack * move.power) / other.defense
+            slow_print("It's not very effective...")
+        else:
+            other.hp -= (self.attack * move.power) / other.defense
+
+        move.pp -= 1
+        if other.hp <= 0:
+            other.hp = 0
+            slow_print(f'{other.name} has fainted')
+            return False
+
+        return True
+
+
+class Attack:
+    def __init__(self, name: str, power: int, max_pp: int, elemental_type: str):
+        self.name = name
+        self.power = power
+        self.pp = max_pp
+        self.max_pp = max_pp
+        self.elemental_type = ElementalType(elemental_type)
+    def __repr__(self):
+        return self.name
+
+
+class ElementalType:
+    def __init__(self, element: str):
+        possible = ['Grass', 'Fire', 'Water', 'Normal']
+        if element.capitalize() not in possible:
+            raise ValueError(f"{element.capitalize()} not available")
+        else:
+            self.element = element.capitalize()
+        self.winner = [('Grass', 'Water'),
+                       ('Fire', 'Grass'),
+                       ('Water', 'Fire')]
+    
+    def __gt__(self, other):
+        return (self.element, other.element) in self.winner
+    
+    def __eq__(self, other):
+        return self.element == other.element
+    
+    def __lt__(self, other):
+        return (other > self)
+
+
+if __name__ == '__main__':
+
+    dark = ElementalType('Dark')
+    grass = ElementalType('Grass')
+    water = ElementalType('Water')
+    print('Pokemon.py has been called')
+    print(len([1, 2, 3]))
+    print(len(grass))
--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-from Pokemon import Pokemon
-from Pokemon import Attack
-from Pokemon import slow_print
-import time
+from Game import *
 import os
 
-pound = Attack('Pound', 40, 30, 'Normal')
-ember = Attack('Ember', 40, 20, 'Fire')
-vine_whip = Attack('Vine Whip', 45, 25, 'Grass')
+pound = Pokemon.Attack('Pound', 40, 30, 'Normal')
+ember = Pokemon.Attack('Ember', 40, 20, 'Fire')
+vine_whip = Pokemon.Attack('Vine Whip', 45, 25, 'Grass')
 charmander_moveset = [pound, ember]
 bulbasaur_moveset = [pound, vine_whip]
 
-charmander = Pokemon(200, 'Charmander', 30, 20, 'Fire', moves=charmander_moveset)
-bulbasaur = Pokemon(200, 'Bulbasaur', 30, 20, 'Grass', moves=bulbasaur_moveset)
+charmander = Pokemon.Pokemon(200, 'Charmander', 30, 20, 'Fire', moves=charmander_moveset)
+bulbasaur = Pokemon.Pokemon(200, 'Bulbasaur', 30, 20, 'Grass', moves=bulbasaur_moveset)
 
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -19,13 +16,12 @@ while True:
     flag = charmander.fight(bulbasaur)
     if flag == False:
         break
-    slow_print(f'{bulbasaur.name} has {bulbasaur.hp} points left\n')
-    time.sleep(1.5)
+    Pokemon.slow_print(f'{bulbasaur.name} has {bulbasaur.hp} points left\n')
+    input('Press Enter to continue')
     os.system('cls' if os.name == 'nt' else 'clear')
     print(f'What will {bulbasaur.name} do?')
     flag = bulbasaur.fight(charmander)
     if flag == False:
         break
-    slow_print(f'{charmander.name} has {charmander.hp} points left\n')
-    time.sleep(1.5)
-    
+    Pokemon.slow_print(f'{charmander.name} has {charmander.hp} points left\n')
+    input('Press Enter to continue')
